00a96d6c36bf63432c3e0731d0d41e28ab94a586
Vitaliy <vitaliysidorov@yandex.ru>
2023-05-22 21:40:09 +0500

    Add Solution Task 2

---
+++ task_2.py
@@ -0,0 +1,26 @@
+# Задача 2: Найдите сумму цифр трехзначного числа.
+
+import os
+
+def clear_console(): # очистка консоли
+  os.system('cls')
+
+clear_console()
+
+def getNumber_ (): # проверка правильности ввода
+    while True:
+        getNumber = input('Введите любое натуральное число: ')  # Ввод числа
+        if getNumber.isdigit() and int(getNumber) !=0: return getNumber
+        else: print(f"Натуральные числа - числа, возникающие естественным образом при счете (1, 2, 3 и так далее...). {getNumber} - не натуральное число.")
+
+print('Программа нахождения суммы цифр в числе.')
+n = int(getNumber_())
+number = n
+summa = 0
+
+while n> 0: # разбиение числа на цифры по разрядам
+    x = n % 10
+    summa = summa+x
+    n = n //10
+    
+print(f'Сумма цифр числа {number} = {summa}')
